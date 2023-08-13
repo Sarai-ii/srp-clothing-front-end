1727a9d08d023ef6238b133272cd6c7d3dfdc624
JenniferEinstein <jennifereinstein@gmail.com>
2023-08-13 10:38:14 -0400

    Got a Japanese Maple photo to use as hero image for home page. FontFamily Clarkson. Tried using bottons and useNavigate but it is not looking good yet.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,51 @@
+import { useNavigate } from "react-router-dom"
+import React from "react";
+import photo from '../Photos/JapaneseMaple.jpg'
+
+
 export default function Home() {
+    const navigate = useNavigate();
+    const fullScreenImage = {
+        backgroundImage: `url(${photo})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontFamily: "'Clarkson',Helvetica,sans-serif",
+      };
+
+      const headingStyle = {
+        color: 'rgb(33, 209, 146)',
+        padding: '10px',
+        position: 'absolute',
+        top: '20%',
+        left: '40%',
+        transform: 'translate(-50%, -50%)',
+        fontSize: '48px',
+      };
+
     return (
         <div className="Home">
-            <h2>Welcome To Extravaganza</h2>
+            <div style={fullScreenImage}>
+            <h2 style={headingStyle}>Welcome to Extravaganza!</h2>
+
+
+            </div>
         </div>
     )
 }
+
+
+/*    ========= CONSIDER USING THESE ===============
+            <button className='all-items-btn' onClick = {() => navigate("/clothing")}>
+                See All Clothing
+            </button>
+            <button className="mens-clothes-btn" onClick = {() => navigate('clothing/men')}>
+                See All Men's Clothing
+            </button>
+            <button className="mens-clothes-btn" onClick = {() => navigate('clothing/men')}>
+                See All Women's Clothing
+            </button>
+*/
--- src/Pages/New.js
+++ src/Pages/New.js
@@ -3,6 +3,7 @@ import NewClothing from "../Components/NewClothing"
 export default function New() {
     return (
         <div className="New">
+            <h2>Enter new clothing item here.</h2>
             <NewClothing />
         </div>
     )
