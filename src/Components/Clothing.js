be12241a80204ba95c6b4a65fa15100c056b295d
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-08-13 13:57:35 -0400

    Struggling with the Images Pern

--- src/Components/Clothing.js
+++ src/Components/Clothing.js
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
+// import Images from "./Images";
 
-export default function Clothing({ clothes }) {
+export default function Clothing({ clothes, images }) {
+  console.log(clothes.clothing_id)
+  console.log(images)
   return (
     <div>
-
-    <tr>
-      <td>
-      <Link to={`/clothes/${clothes.clothing_id}`}>{clothes.name}</Link>
-      </td>
-      <td>
-      <Link to={`/clothes/${clothes.clothing_id}`}>{clothes.cost}</Link>
-      </td>
-      {/* <td>
-        <Link to={`/clothing/${clothing.clothes_id}`}>{clothing.name}</Link>
-      </td> */}
-    </tr>
+      <main className="card">
+        <img src="../Photos/ankle-boots.jpg" className="card-img-top" alt="Item"/>
+        <Link to={`/clothes/${clothes.clothing_id}`}>{clothes.name}</Link>
+        <div className="card-body">
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </main>
     </div>
   );
 }
