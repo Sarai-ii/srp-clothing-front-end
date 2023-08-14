0e221e87a2e6d2c7c578ea2fbcafc4d38b9ebd8e
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-08-14 16:22:47 -0400

    Show page is completed

--- src/Components/ClothingDetails.js
+++ src/Components/ClothingDetails.js
@@ -1,42 +1,45 @@
-// PACKAGES AND REACT DEPENDENCIES
-
-import axios from "axios";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
-
-//STYLING
-import "../Css/Show.css"
+import axios from "axios";
+import "../Css/Show.css";
 
 const API = process.env.REACT_APP_API_URL;
 
 function ClothingDetails() {
-
   const [clothing, setClothing] = useState([]);
   const [isConfirmationVisible, setIsConfirmationVisible] = useState(false);
+  const [showProductDetails, setShowProductDetails] = useState(false);
   let { id } = useParams();
   let navigate = useNavigate();
 
   useEffect(() => {
     axios
-    .get(`${API}/clothing/${id}`)
-    .then((res) => setClothing(res.data))
-    .catch((c) => console.error("The get functionality of this code is not working. Make sure to check the API and if the url pathway is correct."));
+      .get(`${API}/clothing/${id}`)
+      .then((res) => setClothing(res.data))
+      .catch((c) =>
+        console.error(
+          "The get functionality of this code is not working. Make sure to check the API and if the URL pathway is correct."
+        )
+      );
   }, [id, navigate]);
 
   const deleteClothing = () => {
     axios
-    .delete(`${API}/clothing/${id}`)
-    .then(() => navigate(`/new-this-season`))
-    .catch((c) => console.error("catch", c));
+      .delete(`${API}/clothing/${id}`)
+      .then(() => navigate(`/new-this-season`))
+      .catch((c) => console.error("catch", c));
   };
 
+  const toggleDetails = () => {
+    setShowProductDetails(!showProductDetails);
+  };
   const showConfirmationModal = () => {
-    console.log("working modal")
+    console.log("working modal");
     setIsConfirmationVisible(true);
   };
 
   const hideConfirmationModal = () => {
-    console.log("hiding modal")
+    console.log("hiding modal");
     setIsConfirmationVisible(false);
   };
 
@@ -47,39 +50,90 @@ function ClothingDetails() {
 
   return (
     <div className="">
-      <article className="card card-details text-center bg-dark grey ">
-        <h4>{clothing.name}</h4>
-        <h6>${clothing.cost}</h6>
-        <div className="showNavigation">
-          <div className="">
-            {" "}
-            <Link to={`/new-this-season`}>
-              <button className="btn btn-outline-secondary button-details">Back</button>
-            </Link>
+      <article className="show card card-details text-center bg-dark grey">
+        <div className="row">
+          <div className="col details-left ">
+            {/* Image on the left */}
+            <img
+              src={clothing.image}
+              className="details-img card-img-top product-shot"
+              alt={clothing.name}
+            />
           </div>
-          <div>
-            <Link to={`/clothes/${id}/edit`}>
-              <button className="btn btn-outline-secondary button-details">Edit</button>
-            </Link>
-          </div>
-          <div className="">
-            <button
-              className="btn btn-outline-secondary button-details "
-              onClick={showConfirmationModal}>Delete
-            </button>
-            {isConfirmationVisible && (
-               <div className="modal">
-                <div className="modal-content">
-                  <p>Are you sure you want to delete?</p>
-                  <button onClick={handleDelete}>Yes, Delete</button>
-                  <button onClick={hideConfirmationModal}>Cancel</button>
+          <div className="col">
+
+            {/* Content on the right */}
+            <div className="details-right card-body">
+              <div className="details-body">
+                <h6>{clothing.clothing_id}</h6>
+                <h4>{clothing.name}</h4>
+                <h6>${clothing.cost}</h6>
+                <button className=" details-btn btn btn-default">Place in Cart</button>
+                <ul className="details-body">
+                  <h5>Find in Store</h5>
+                  <h5 className="details-body" onClick={toggleDetails}>Product Details</h5>
+                  {showProductDetails && (
+                    <ul className="details-body" >PRODUCT CARE 
+                    <li>{clothing.handwash_only ? 
+                      <span className="details-body" >
+                        <svg xmlns="" width="26" height="26" fill="#f0dada" class="bi bi-badge-wc-fill" viewBox="0 0 16 16">
+                          <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm11.666 1.89c.682 0 1.139.47 1.187 1.107H14v-.11c-.053-1.187-1.024-2-2.342-2-1.604 0-2.518 1.05-2.518 2.751v.747c0 1.7.905 2.73 2.518 2.73 1.314 0 2.285-.792 2.342-1.939v-.114h-1.147c-.048.615-.497 1.05-1.187 1.05-.839 0-1.318-.62-1.318-1.727v-.742c0-1.112.488-1.754 1.318-1.754zm-6.188.926h.044L6.542 11h1.006L9 5.001H7.818l-.82 4.355h-.056L5.97 5.001h-.94l-.972 4.355h-.053l-.827-4.355H2L3.452 11h1.005l1.02-4.184z"/>
+                        </svg> Handwash Only
+                    </span> : null} 
+                    </li>
+                    <li>Avoid contact with hand cream, hand sanitizer, make-up and perfume.</li>
+                    <li>For any enquiries about your product, please do not hesitate to contact ant Sen Extravaganza.</li>
+                    <h6>This reference is Made in {clothing.country}</h6>
+                    <ul className="details-body">MATERIALS
+                      <li>
+                        {clothing.material}
+                      </li>
+                    </ul>
+                  </ul>
+                )}
+                <h5>Delivery & Return</h5>
+                </ul>
+              </div>
+              <div className="showNavigation">
+                <div className="">
+                  {" "}
+                  <Link to={`/new-this-season`}>
+                    <button className="btn btn-outline-secondary button-details">
+                      Back
+                    </button>
+                  </Link>
                 </div>
-             </div>
-           )}
+                <div>
+                  <Link to={`/clothes/${id}/edit`}>
+                    <button className="btn btn-outline-secondary button-details">
+                      Edit
+                    </button>
+                  </Link>
+                </div>
+                <div className="">
+                  <button
+                    className="btn btn-outline-secondary button-details "
+                    onClick={showConfirmationModal}
+                  >
+                    Delete
+                  </button>
+                  {isConfirmationVisible && (
+                    <div className="modal">
+                      <div className="modal-content">
+                        <p>Are you sure you want to delete?</p>
+                        <button onClick={handleDelete}>Yes, Delete</button>
+                        <button onClick={hideConfirmationModal}>Cancel</button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </article>
     </div>
   );
 }
+
 export default ClothingDetails;
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -57,12 +57,18 @@ export default function ToggleableSidebar(clothing) {
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
     >
+        
         <div className="sidebar-toggle" onClick={toggleSidebar}>
-            Home/New-this-season/clothing
+            Hover For A More Detailed 
+            <br/>Shopping Experience 
+            <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="black" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8Zm-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5Z"/>
+            </svg>
         </div>
         <div className="sidebar-content">
             {/* Sidebar content */}
             <ul className="dropdown-menus">
+                <p className='mb-4'><strong>Home/New-this-season/clothing</strong></p>
                 <p>Filtered by: </p>
                 <button className='btn btn-default'>Clear all</button>
                 <hr />
@@ -192,6 +198,20 @@ export default function ToggleableSidebar(clothing) {
                                     type="checkbox"
                                     id="checkbox"
                                     />
+                                    Diamond
+                                </li>
+                                <li>
+                                    <input
+                                    type="checkbox"
+                                    id="checkbox"
+                                    />
+                                    Elastane
+                                </li>
+                                <li>
+                                    <input
+                                    type="checkbox"
+                                    id="checkbox"
+                                    />
                                     Gold-Plated
                                 </li>
                                 <li>
@@ -213,14 +233,14 @@ export default function ToggleableSidebar(clothing) {
                                     type="checkbox"
                                     id="checkbox"
                                     />
-                                    Metal
+                                    Nubuck
                                 </li>
                                 <li>
                                     <input
                                     type="checkbox"
                                     id="checkbox"
                                     />
-                                    Nubuck
+                                    Nylon
                                 </li>
                                 <li>
                                     <input
@@ -256,6 +276,13 @@ export default function ToggleableSidebar(clothing) {
                                     id="checkbox"
                                     />
                                     Silk
+                                </li>
+                                <li>
+                                    <input
+                                    type="checkbox"
+                                    id="checkbox"
+                                    />
+                                    Sterling Silver
                                 </li>
                                 <li>
                                     <input
